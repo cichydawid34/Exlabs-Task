2d62d488d4974cbb6672617ee5185a6bdf5a7e12
Dawid Cichy <dawidcichy34@gmail.com>
2023-03-31 15:49:22 +0200

    Add: Header Component

---
+++ beertask/src/components/headerComponent.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <>
+      <div className="relative h-56 z-0 justify-center items-center overflow-hidden">
+        <div className="absolute bg-gradient-to-r from-[#715f92]  to-[#97afdd]   h-80   w-[100em] -rotate-6 -top-48 -left-16 -z-10"></div>
+        <h1 className="text-4xl font-bold text-center text-[#FFA418] mt-14">
+          Beers
+        </h1>
+      </div>
+    </>
+  );
+}
